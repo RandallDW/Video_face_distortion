c2590d66e526339fae435e76ea3145a328563d20
Randall <dong0125@vt.edu>
2017-11-27 23:38:45 -0500

    add spherize filter

---
+++ Code/distortion.py
@@ -0,0 +1,31 @@
+import math
+from PIL import Image
+def spherize(img):
+    width, height = img.size
+    
+    mid_x = width / 2
+    mid_y = height / 2
+    max_mid_xy = max(mid_x, mid_y)
+    pix = img.load()
+    
+    dst_img = Image.new("RGBA", (width, height))
+    dst_pix = dst_img.load()
+    
+    for w in xrange(width):
+        for h in xrange(height):
+            offset_x = w - mid_x
+            offset_y = h - mid_y
+            
+            radian = math.atan2(offset_y, offset_x)
+            radius = (offset_x ** 2 + offset_y ** 2) / max_mid_xy
+            
+            x = int(radius * math.cos(radian)) + mid_x
+            y = int(radius * math.sin(radian)) + mid_y
+            
+            x = min(max(x, 0), width - 1)
+            y = min(max(y, 0), height - 1)
+            
+            dst_pix[w, h] = pix[x, y]
+            
+    return dst_img
+
